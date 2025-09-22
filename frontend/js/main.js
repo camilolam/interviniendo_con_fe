73ba8144853c8924e418e7057728b9252d4bccf4
Wilson Camilo Cañavaral Rodriguez <camilo.lam93@gmail.com>
2025-09-22 16:41:48 -0500

    Creación de la modal, e incrustación del video desde youtube

--- frontend/js/main.js
+++ frontend/js/main.js
@@ -1,1 +1,19 @@
+const video = document.querySelector(".video-modal");
 
+
+function abrirModal(idModal) {
+  const modal = document.getElementById(idModal);
+  if (modal) {
+    modal.classList.add('modal-open');
+    video.src = "https://www.youtube.com/embed/UnY6bGhSLRo?si=Gg4eLF8k_aF7_AMY";
+  }
+}
+
+function cerrarModal(idModal) {
+  const modal = document.getElementById(idModal);
+  
+  if (modal) {
+    modal.classList.remove('modal-open');
+    video.src = "";
+  }
+}
